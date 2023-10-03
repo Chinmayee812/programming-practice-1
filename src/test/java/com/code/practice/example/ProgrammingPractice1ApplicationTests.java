6a36ea297cfb7f019b39fb54427dee884313917b
Chinmayee812 <gmchinmayee@gmail.com>
2023-10-03 13:31:57 -0500

    Practice Programming

---
+++ src/test/java/com/code/practice/example/ProgrammingPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.practice.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingPractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
